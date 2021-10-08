f8fb1ef5b5d3ff5e4a30b2e9c20da838c6fe7546
McT <ian.mctavish@tldsb.on.ca>
2021-10-08 16:37:58 -0400

    Fixed Evin's overwrite

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,9 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-<<<<<<< HEAD
 //Evin Park - October 8, 2021
+//Ian McTavish
 
-=======
-// Ian McTavish - Oct 8, 2021
->>>>>>> f43387aad9f53b03d490108fa876ba6b800701a0
 package frc.robot;
 
 /**
