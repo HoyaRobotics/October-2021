ea8cfd0afa730d63a6aaaf059db187559f27372e
HoyaRobotics <hoyarobotics@gmail.com>
2021-10-18 15:11:11 -0400

    Added drivebase info

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants 
 {
@@ -22,10 +26,16 @@ public final class Constants
     //Ethan - add the USB ids
     //Sophia - CANbus ids - for drive motors
 
+public static class Controls {
+// Driver controls:
+public static final Hand DRIVE = Hand.kLeft;
+}
+// USB ids
+public static int DRIVER = 0;
 
 // CANbus ids
-public static final int FRONT_LEFT_DRIVE = 0;
-public static final int FRONT_RIGHT_DRIVE = 1; 
+public static final int FRONT_LEFT_DRIVE = 31;
+public static final int FRONT_RIGHT_DRIVE = 30; 
 //We'll uncomment as we set up the different motors.  Currently only the two drive train motors are wired.
     //Need to check the CANBus id on the motor and set it.
 /*public static final int REAR_LEFT_DRIVE = 2; 
--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -4,7 +4,8 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -17,13 +18,23 @@ public class DriveBase extends SubsystemBase {
   private final Talon rightLeader = new Talon(Constants.FRONT_RIGHT_DRIVE);
 
   private final DifferentialDrive drive = new DifferentialDrive(leftLeader, rightLeader);
+  public DriveBase() {
+    leftLeader.configFactoryDefault();
+    rightLeader.configFactoryDefault();
 
+    SupplyCurrentLimitConfiguration supplyLimit = new SupplyCurrentLimitConfiguration(true, 30, 35, 1.0);
+    leftLeader.configSupplyCurrentLimit (supplyLimit);
+    rightLeader.configSupplyCurrentLimit (supplyLimit);
+
+    leftLeader.setNeutralMode(NeutralMode.Coast);
+    rightLeader.setNeutralMode(NeutralMode.Coast);
+  }
   public void arcadeDrive (double throttle, double rotation){
     rotation *= -1;
 
     drive.arcadeDrive(throttle, rotation);
   }
-  public DriveBase() {}
+  
 
   @Override
   public void periodic() {
